592fbb91c01835aabe5288b0bd63431e200e03eb
eeelian8 <elianleguizamon@gmail.com>
2026-02-13 12:35:36 -0300

    1ra Etapa de organizacion del proyecto, maquetacion y db.json

---
+++ src/users/presentation/render-table/render-table.js
@@ -0,0 +1,33 @@
+import './render-table.css';
+import usersStore from '../../store/users-store';
+
+let table;
+const createTable = () => {
+    const table = document.createElement('table');
+    const tableHeaders = document.createElement('thead');
+    tableHeaders.innerHTML = `
+    <tr>
+        <th>#ID</th>
+        <th>Balance</th>
+        <th>FristName</th>
+        <th>LastName</th>
+        <th>Active</th>
+        <th>Actions</th>
+    </tr>
+    `;
+
+    const tableBody = document.createElement('tbody');
+    table.append(tableHeaders, tableBody)
+    return table;
+
+}
+
+export const renderTable = (element) => {
+    const users = usersStore.getUsers();
+    if (!table) {
+        table = createTable();
+    }
+}
+
+
+
