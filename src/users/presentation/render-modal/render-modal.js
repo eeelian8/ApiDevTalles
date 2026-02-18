5963852e54a5cd4e9e9fbe914374454d3b1aae05
eeelian8 <elianleguizamon@gmail.com>
2026-02-18 01:08:03 -0300

    Funcion del formulario, mejoras en front, cambios en el db-json y cambios en la funcion de los buttons

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -1,7 +1,8 @@
 import "./render-modal.css";
 
 import modalHtml from "./render-modal.html?raw";
-let modal;
+
+let modal, form;
 
 export const showModal = () => {
   modal?.classList.remove("hide-modal");
@@ -9,16 +10,49 @@ export const showModal = () => {
 
 export const hideModal = () => {
   modal?.classList.add("hide-modal");
+  form?.reset();
 };
 
 /**
  * @param {HTMLDivElement} element
+ * @param {(userLike)=>Promise<void>} callback
  */
-export const renderModal = (element) => {
+export const renderModal = (element, callback) => {
   if (modal) return;
   modal = document.createElement("div");
   modal.innerHTML = modalHtml;
   modal.className = "modal-container hide-modal";
+
+  form = modal.querySelector("form");
+
+  modal.addEventListener("click", (event) => {
+    if (event.target.className === "modal-container") {
+      hideModal();
+    }
+  });
+
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const userLike = {};
+
+    for (const [key, value] of formData) {
+      if (key === "balance") {
+        userLike[key] = +value; //conversion a Number
+        continue;
+      }
+
+      if (key === "isActive") {
+        userLike[key] = value === "on" ? true : false;
+        continue;
+      }
+
+      userLike[key] = value;
+    }
+
+    await callback(userLike);
+    hideModal();
+  });
 
   element.append(modal);
 };
--- src/users/users-app.js
+++ src/users/users-app.js
@@ -3,6 +3,7 @@ import { renderTable } from "./presentation/render-table/render-table";
 import { renderButtons } from "./presentation/render-buttons/render-buttons";
 import { renderAddButtons } from "./presentation/render-add-buttons/render-add-buttons";
 import { renderModal } from "./presentation/render-modal/render-modal";
+import { saveUser } from "./use-cases/save-user";
 
 export const UsersApp = async (element) => {
   element.innerHTML = "Loading...";
@@ -11,8 +12,10 @@ export const UsersApp = async (element) => {
 
   renderTable(element);
   renderButtons(element);
-  renderAddButtons(element, () => {
-    console.log("Fab button clicked");
+  renderAddButtons(element);
+  renderModal(element, async (userLike) => {
+    const user = await saveUser(userLike);
+    usersStore.onUserChanged(user);
+    renderTable();
   });
-  renderModal(element);
 };
